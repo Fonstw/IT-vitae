79242f5d46ea68c082e2cb1aa5d16d1cc6b9e237
Fonzie! <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2019-10-04 15:49:22 +0200

    Finished green 6

---
+++ Groen/06-Camping/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace _06_Camping
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMeters = new System.Windows.Forms.Label();
+            this.lblPersonen = new System.Windows.Forms.Label();
+            this.lblHonden = new System.Windows.Forms.Label();
+            this.lblAutos = new System.Windows.Forms.Label();
+            this.lblBegin = new System.Windows.Forms.Label();
+            this.lblEind = new System.Windows.Forms.Label();
+            this.txbMeters = new System.Windows.Forms.TextBox();
+            this.numPersonen = new System.Windows.Forms.NumericUpDown();
+            this.numHonden = new System.Windows.Forms.NumericUpDown();
+            this.numAutos = new System.Windows.Forms.NumericUpDown();
+            this.dtpBegin = new System.Windows.Forms.DateTimePicker();
+            this.dtpEind = new System.Windows.Forms.DateTimePicker();
+            this.btnBereken = new System.Windows.Forms.Button();
+            this.lblAntwoord = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numPersonen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHonden)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAutos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMeters
+            // 
+            this.lblMeters.AutoSize = true;
+            this.lblMeters.Location = new System.Drawing.Point(12, 15);
+            this.lblMeters.Name = "lblMeters";
+            this.lblMeters.Size = new System.Drawing.Size(122, 17);
+            this.lblMeters.TabIndex = 0;
+            this.lblMeters.Text = "Meters plaatshuur";
+            // 
+            // lblPersonen
+            // 
+            this.lblPersonen.AutoSize = true;
+            this.lblPersonen.Location = new System.Drawing.Point(12, 42);
+            this.lblPersonen.Name = "lblPersonen";
+            this.lblPersonen.Size = new System.Drawing.Size(112, 17);
+            this.lblPersonen.TabIndex = 1;
+            this.lblPersonen.Text = "Aantal personen";
+            // 
+            // lblHonden
+            // 
+            this.lblHonden.AutoSize = true;
+            this.lblHonden.Location = new System.Drawing.Point(12, 70);
+            this.lblHonden.Name = "lblHonden";
+            this.lblHonden.Size = new System.Drawing.Size(100, 17);
+            this.lblHonden.TabIndex = 2;
+            this.lblHonden.Text = "Aantal honden";
+            // 
+            // lblAutos
+            // 
+            this.lblAutos.AutoSize = true;
+            this.lblAutos.Location = new System.Drawing.Point(12, 98);
+            this.lblAutos.Name = "lblAutos";
+            this.lblAutos.Size = new System.Drawing.Size(90, 17);
+            this.lblAutos.TabIndex = 3;
+            this.lblAutos.Text = "Aantal auto\'s";
+            // 
+            // lblBegin
+            // 
+            this.lblBegin.AutoSize = true;
+            this.lblBegin.Location = new System.Drawing.Point(12, 129);
+            this.lblBegin.Name = "lblBegin";
+            this.lblBegin.Size = new System.Drawing.Size(83, 17);
+            this.lblBegin.TabIndex = 4;
+            this.lblBegin.Text = "Begindatum";
+            // 
+            // lblEind
+            // 
+            this.lblEind.AutoSize = true;
+            this.lblEind.Location = new System.Drawing.Point(12, 157);
+            this.lblEind.Name = "lblEind";
+            this.lblEind.Size = new System.Drawing.Size(75, 17);
+            this.lblEind.TabIndex = 5;
+            this.lblEind.Text = "Einddatum";
+            // 
+            // txbMeters
+            // 
+            this.txbMeters.Location = new System.Drawing.Point(140, 12);
+            this.txbMeters.Name = "txbMeters";
+            this.txbMeters.Size = new System.Drawing.Size(100, 22);
+            this.txbMeters.TabIndex = 6;
+            // 
+            // numPersonen
+            // 
+            this.numPersonen.Location = new System.Drawing.Point(140, 40);
+            this.numPersonen.Name = "numPersonen";
+            this.numPersonen.Size = new System.Drawing.Size(120, 22);
+            this.numPersonen.TabIndex = 7;
+            // 
+            // numHonden
+            // 
+            this.numHonden.Location = new System.Drawing.Point(140, 68);
+            this.numHonden.Name = "numHonden";
+            this.numHonden.Size = new System.Drawing.Size(120, 22);
+            this.numHonden.TabIndex = 8;
+            // 
+            // numAutos
+            // 
+            this.numAutos.Location = new System.Drawing.Point(140, 96);
+            this.numAutos.Name = "numAutos";
+            this.numAutos.Size = new System.Drawing.Size(120, 22);
+            this.numAutos.TabIndex = 9;
+            // 
+            // dtpBegin
+            // 
+            this.dtpBegin.Location = new System.Drawing.Point(140, 124);
+            this.dtpBegin.Name = "dtpBegin";
+            this.dtpBegin.Size = new System.Drawing.Size(200, 22);
+            this.dtpBegin.TabIndex = 10;
+            // 
+            // dtpEind
+            // 
+            this.dtpEind.Location = new System.Drawing.Point(140, 152);
+            this.dtpEind.Name = "dtpEind";
+            this.dtpEind.Size = new System.Drawing.Size(200, 22);
+            this.dtpEind.TabIndex = 11;
+            // 
+            // btnBereken
+            // 
+            this.btnBereken.Location = new System.Drawing.Point(140, 180);
+            this.btnBereken.Name = "btnBereken";
+            this.btnBereken.Size = new System.Drawing.Size(75, 23);
+            this.btnBereken.TabIndex = 12;
+            this.btnBereken.Text = "Bereken";
+            this.btnBereken.UseVisualStyleBackColor = true;
+            this.btnBereken.Click += new System.EventHandler(this.BtnBereken_Click);
+            // 
+            // lblAntwoord
+            // 
+            this.lblAntwoord.AutoSize = true;
+            this.lblAntwoord.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblAntwoord.Location = new System.Drawing.Point(12, 212);
+            this.lblAntwoord.Name = "lblAntwoord";
+            this.lblAntwoord.Size = new System.Drawing.Size(48, 17);
+            this.lblAntwoord.TabIndex = 13;
+            this.lblAntwoord.Text = "€ 0,00";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 238);
+            this.Controls.Add(this.lblAntwoord);
+            this.Controls.Add(this.btnBereken);
+            this.Controls.Add(this.dtpEind);
+            this.Controls.Add(this.dtpBegin);
+            this.Controls.Add(this.numAutos);
+            this.Controls.Add(this.numHonden);
+            this.Controls.Add(this.numPersonen);
+            this.Controls.Add(this.txbMeters);
+            this.Controls.Add(this.lblEind);
+            this.Controls.Add(this.lblBegin);
+            this.Controls.Add(this.lblAutos);
+            this.Controls.Add(this.lblHonden);
+            this.Controls.Add(this.lblPersonen);
+            this.Controls.Add(this.lblMeters);
+            this.Name = "frmMain";
+            this.Text = "Camping";
+            ((System.ComponentModel.ISupportInitialize)(this.numPersonen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHonden)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numAutos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMeters;
+        private System.Windows.Forms.Label lblPersonen;
+        private System.Windows.Forms.Label lblHonden;
+        private System.Windows.Forms.Label lblAutos;
+        private System.Windows.Forms.Label lblBegin;
+        private System.Windows.Forms.Label lblEind;
+        private System.Windows.Forms.TextBox txbMeters;
+        private System.Windows.Forms.NumericUpDown numPersonen;
+        private System.Windows.Forms.NumericUpDown numHonden;
+        private System.Windows.Forms.NumericUpDown numAutos;
+        private System.Windows.Forms.DateTimePicker dtpBegin;
+        private System.Windows.Forms.DateTimePicker dtpEind;
+        private System.Windows.Forms.Button btnBereken;
+        private System.Windows.Forms.Label lblAntwoord;
+    }
+}
+
---
+++ Groen/06-Camping/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_Camping
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBereken_Click(object sender, EventArgs e)
+        {
+            float price = 0;
+
+            float meters = 0;
+            if (txbMeters.Text != "")
+            {
+                if (!float.TryParse(txbMeters.Text, out meters))
+                {
+                    lblAntwoord.Text = "Voer a.u.b. geldige getallen in.";
+                    return;
+                }
+            }
+
+            int people = Convert.ToInt32(numPersonen.Value);
+            int dogs = Convert.ToInt32(numHonden.Value);
+            int cars = Convert.ToInt32(numAutos.Value);
+
+            int daysOutOfSeason = 0, daysInSeason = 0;
+            DateTime beginDate = dtpBegin.Value;
+            DateTime endDate = dtpEind.Value;
+            DateTime seasonBegin = new DateTime(DateTime.Now.Year, 7, 11);
+            DateTime seasonEnd = new DateTime(DateTime.Now.Year, 8, 15);
+
+            if (beginDate < seasonBegin && endDate < seasonBegin)
+                daysOutOfSeason = (endDate - beginDate).Days;
+            else if (beginDate > seasonEnd && endDate > seasonEnd)
+                // this is the same, they are separated for readability
+                daysOutOfSeason = (endDate - beginDate).Days;
+            else if (beginDate >= seasonBegin && endDate <= seasonEnd)
+                daysInSeason = (endDate - beginDate).Days;
+            else if (beginDate < seasonBegin && endDate > seasonEnd)
+            {
+                daysInSeason = (seasonEnd - seasonBegin).Days;
+                daysOutOfSeason = (endDate - beginDate).Days;
+                daysOutOfSeason -= daysInSeason;
+            }
+            else if (beginDate < seasonBegin && endDate <= seasonEnd)
+            {
+                daysInSeason = (endDate - seasonBegin).Days;
+                daysOutOfSeason = (seasonBegin - beginDate).Days;
+            }
+            else if (beginDate >= seasonBegin && endDate > seasonEnd)
+            {
+                daysInSeason = (seasonEnd - beginDate).Days;
+                daysOutOfSeason = (endDate - seasonEnd).Days;
+            }
+
+            int totalDays = (endDate - beginDate).Days;
+
+            // these are all pays per day regardless
+            price += people * 5;
+            price += dogs * 4;
+            price += cars * 6;
+            // so there you go!
+            price *= totalDays;
+
+            // the amount of meters under 10 are €2 discount per meter regardless
+            if (meters < 10)
+                price -= totalDays * (10 - meters) * 2;
+            // the amount of meters above 10 cost €3 extra regardless
+            else if (meters > 10)
+                price += totalDays * (meters - 10) * 3;
+
+            // baseprice for the meters will be €25/€30 depending on the day
+            price += daysOutOfSeason * 25;
+            price += daysInSeason * 30;
+
+            lblAntwoord.Text = "€ " + price.ToString("N2");
+        }
+    }
+}
