68aef189e12155a38f75507912ed098887c57903
Fonzie! <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2019-10-04 12:17:07 +0200

    Finished second green assignment

---
+++ Groen/02-Ouderbrijdrage_school/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace _02_Ouderbrijdrage_school
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOnderTien = new System.Windows.Forms.Label();
+            this.lblTienOfOuder = new System.Windows.Forms.Label();
+            this.lblEenOuderGezin = new System.Windows.Forms.Label();
+            this.txbOnderTien = new System.Windows.Forms.TextBox();
+            this.txbTienOfOuder = new System.Windows.Forms.TextBox();
+            this.ckbEenOuderGezin = new System.Windows.Forms.CheckBox();
+            this.lblAntwoord = new System.Windows.Forms.Label();
+            this.btnBereken = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOnderTien
+            // 
+            this.lblOnderTien.AutoSize = true;
+            this.lblOnderTien.Location = new System.Drawing.Point(12, 15);
+            this.lblOnderTien.Name = "lblOnderTien";
+            this.lblOnderTien.Size = new System.Drawing.Size(168, 17);
+            this.lblOnderTien.TabIndex = 0;
+            this.lblOnderTien.Text = "Aantal kinderen onder 10";
+            // 
+            // lblTienOfOuder
+            // 
+            this.lblTienOfOuder.AutoSize = true;
+            this.lblTienOfOuder.Location = new System.Drawing.Point(12, 43);
+            this.lblTienOfOuder.Name = "lblTienOfOuder";
+            this.lblTienOfOuder.Size = new System.Drawing.Size(184, 17);
+            this.lblTienOfOuder.TabIndex = 1;
+            this.lblTienOfOuder.Text = "Aantal kinderen 10 of ouder";
+            // 
+            // lblEenOuderGezin
+            // 
+            this.lblEenOuderGezin.AutoSize = true;
+            this.lblEenOuderGezin.Location = new System.Drawing.Point(12, 69);
+            this.lblEenOuderGezin.Name = "lblEenOuderGezin";
+            this.lblEenOuderGezin.Size = new System.Drawing.Size(112, 17);
+            this.lblEenOuderGezin.TabIndex = 2;
+            this.lblEenOuderGezin.Text = "Eénoudergezin?";
+            // 
+            // txbOnderTien
+            // 
+            this.txbOnderTien.Location = new System.Drawing.Point(202, 12);
+            this.txbOnderTien.Name = "txbOnderTien";
+            this.txbOnderTien.Size = new System.Drawing.Size(100, 22);
+            this.txbOnderTien.TabIndex = 3;
+            // 
+            // txbTienOfOuder
+            // 
+            this.txbTienOfOuder.Location = new System.Drawing.Point(202, 40);
+            this.txbTienOfOuder.Name = "txbTienOfOuder";
+            this.txbTienOfOuder.Size = new System.Drawing.Size(100, 22);
+            this.txbTienOfOuder.TabIndex = 4;
+            // 
+            // ckbEenOuderGezin
+            // 
+            this.ckbEenOuderGezin.AutoSize = true;
+            this.ckbEenOuderGezin.Location = new System.Drawing.Point(202, 68);
+            this.ckbEenOuderGezin.Name = "ckbEenOuderGezin";
+            this.ckbEenOuderGezin.Size = new System.Drawing.Size(45, 21);
+            this.ckbEenOuderGezin.TabIndex = 5;
+            this.ckbEenOuderGezin.Text = "Ja";
+            this.ckbEenOuderGezin.UseVisualStyleBackColor = true;
+            // 
+            // lblAntwoord
+            // 
+            this.lblAntwoord.AutoSize = true;
+            this.lblAntwoord.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblAntwoord.Location = new System.Drawing.Point(12, 129);
+            this.lblAntwoord.Name = "lblAntwoord";
+            this.lblAntwoord.Size = new System.Drawing.Size(48, 17);
+            this.lblAntwoord.TabIndex = 7;
+            this.lblAntwoord.Text = "€ 0,00";
+            // 
+            // btnBereken
+            // 
+            this.btnBereken.Location = new System.Drawing.Point(202, 95);
+            this.btnBereken.Name = "btnBereken";
+            this.btnBereken.Size = new System.Drawing.Size(75, 23);
+            this.btnBereken.TabIndex = 6;
+            this.btnBereken.Text = "Bereken";
+            this.btnBereken.UseVisualStyleBackColor = true;
+            this.btnBereken.Click += new System.EventHandler(this.BtnBereken_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 155);
+            this.Controls.Add(this.btnBereken);
+            this.Controls.Add(this.lblAntwoord);
+            this.Controls.Add(this.ckbEenOuderGezin);
+            this.Controls.Add(this.txbTienOfOuder);
+            this.Controls.Add(this.txbOnderTien);
+            this.Controls.Add(this.lblEenOuderGezin);
+            this.Controls.Add(this.lblTienOfOuder);
+            this.Controls.Add(this.lblOnderTien);
+            this.Name = "frmMain";
+            this.Text = "Ouderbijdrage school";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOnderTien;
+        private System.Windows.Forms.Label lblTienOfOuder;
+        private System.Windows.Forms.Label lblEenOuderGezin;
+        private System.Windows.Forms.TextBox txbOnderTien;
+        private System.Windows.Forms.TextBox txbTienOfOuder;
+        private System.Windows.Forms.CheckBox ckbEenOuderGezin;
+        private System.Windows.Forms.Label lblAntwoord;
+        private System.Windows.Forms.Button btnBereken;
+    }
+}
+
---
+++ Groen/02-Ouderbrijdrage_school/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_Ouderbrijdrage_school
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBereken_Click(object sender, EventArgs e)
+        {
+            float price = 50;
+
+            int childrenUnderTen = 0;
+            if (txbOnderTien.Text != "")
+            {
+                if (int.TryParse(txbOnderTien.Text, out childrenUnderTen))
+                    price += childrenUnderTen * 25;
+                else
+                {
+                    lblAntwoord.Text = "Voer a.u.b. een rond getal in.";
+                    return;
+                }
+            }
+
+            int tenOrOlder = 0;
+            if (txbTienOfOuder.Text != "")
+            {
+                if (int.TryParse(txbTienOfOuder.Text, out tenOrOlder))
+                    price += tenOrOlder * 37;
+                else
+                {
+                    lblAntwoord.Text = "Voer a.u.b. een rond getal in.";
+                    return;
+                }
+            }
+
+            if (ckbEenOuderGezin.Checked)
+                price *= .75f;
+
+            if (price > 150)
+                price = 150;
+
+            lblAntwoord.Text = "€ " + price.ToString("N2");
+        }
+    }
+}
